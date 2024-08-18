eac3d1693f24c16abc56dad4eb7637d00ef911c7
Benny <ilvmey@yahoo.com.tw>
2024-08-18 20:41:58 +0800

    finish adjacency station

--- mrt_app/fixtures/raw_data/adjacency_station.py
+++ mrt_app/fixtures/raw_data/adjacency_station.py
@@ -23,6 +23,25 @@ def convert_to_json(rows):
     for station in station_info:
         station_info_dict[station['fields']['code']] = station['pk']
 
+    output = []
+    pk = 1
+    for row in rows:
+        from_station_id = station_info_dict[row['from']]
+        for to_station in row['to']:
+            to_station_id = station_info_dict[to_station]
+            data = {
+                'model': 'mrt_app.adjacencystation',
+                'pk': pk,
+                'fields': {
+                    'origin_station': from_station_id,
+                    'destination_station': to_station_id,
+                }
+            }
+            pk += 1
+            output.append(data)
+    with open('mrt_app/fixtures/adjacency_station.json', mode='w', encoding='utf-8') as file:
+        json.dump(output, file, ensure_ascii=False, indent=4)
+
 
 if __name__ == '__main__':
     rows = read_adjacency_station_info()
